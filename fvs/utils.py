6a3254a4c5f234743ae613ce9f69654d36753b1f
mirkobrombin <send@mirko.pm>
2022-07-17 21:53:31 +0200

    add FVSUtils.get_md5_hash for binary hashing

--- fvs/utils.py
+++ fvs/utils.py
@@ -1,4 +1,5 @@
 import inspect
+import hashlib
 
 
 class FVSUtils:
@@ -11,3 +12,11 @@ class FVSUtils:
         stack = inspect.stack()[2]
         caller = stack[0].f_locals.get('self').__class__.__name__
         return caller
+
+    @staticmethod
+    def get_md5_hash(path: str):
+        """
+        Get the MD5 hash of a file.
+        """
+        with open(path, "rb") as f:
+            return hashlib.md5(f.read()).hexdigest()
