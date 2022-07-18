39f03fd49a1887e288db9bf9fb6098a23ea151d2
mirkobrombin <send@mirko.pm>
2022-07-18 08:57:05 +0200

    calculate md5 hash in chunks

--- fvs/utils.py
+++ fvs/utils.py
@@ -12,11 +12,14 @@ class FVSUtils:
         stack = inspect.stack()[2]
         caller = stack[0].f_locals.get('self').__class__.__name__
         return caller
-
+    
     @staticmethod
-    def get_md5_hash(path: str):
-        """
-        Get the MD5 hash of a file.
-        """
-        with open(path, "rb") as f:
-            return hashlib.md5(f.read()).hexdigest()
+    def get_md5_hash(path: str, block_size: int=2**20):
+        md5_temp = hashlib.md5()
+        with open(path , "rb" ) as f:
+            while True:
+                buffer = f.read(block_size)
+                if not buffer:
+                    break
+                md5_temp.update(buffer)
+        return md5_temp.hexdigest()
