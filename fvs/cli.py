4c68f020ab711b7ca28632c3071d473c7c8d9423
mirkobrombin <send@mirko.pm>
2022-07-21 16:41:22 +0200

    0.2

--- fvs/cli.py
+++ fvs/cli.py
@@ -6,7 +6,7 @@ import contextlib
 from fvs.repo import FVSRepo
 from fvs.exceptions import FVSNothingToCommit, FVSEmptyCommitMessage, FVSStateNotFound, FVSNothingToRestore
 
-version = 'FVS 0.1'
+version = 'FVS 0.2'
 
 
 def fvs_cli():
