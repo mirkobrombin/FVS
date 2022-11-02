0c9e7a4f4aa5a92ed32f4a350f471b3ce319687c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-02 22:56:21 +0000

    Adding tarfile member sanitization to extractall()

--- fvs/file.py
+++ fvs/file.py
@@ -152,7 +152,26 @@ class FVSFile:
             logger.debug(f"restoring file {self.__file_name}")
             os.makedirs(dir_name, exist_ok=True)
             with tarfile.open(file_path, "r:gz") as tar:
-                tar.extractall(dir_name)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, dir_name)
                 os.rename(
                     os.path.join(dir_name, self.__sha1),
                     full_rel_path
